b5d11a0f755bd8e8b85502ba981d0ac9aff87a0b
Eddie Lee <eddielee6@me.com>
2017-03-12 20:28:09 +0000

    Tidy

--- gulpfile.js
+++ gulpfile.js
@@ -20,33 +20,28 @@ gutil.log(`Connecting to ${config.username}@${config.host}`);
 
 const gulpSSH = new GulpSSH({ sshConfig: { host, port, username, password } = config });
 
-gulp.task('clean', () => 
+gulp.task('clean', () =>
     gulpSSH.exec(`rm -r -f ~/${config.dest}`));
 
-gulp.task('transfer:src', () => 
-    gulp
-    .src([
-        './src/**'
-    ])
+gulp.task('transfer:src', () =>
+    gulp.src(['./src/**'])
     .pipe(gulpSSH.dest(`${config.dest}/src`)));
 
-gulp.task('transfer:packagejson', () => 
-    gulp.src([
-        './package.json'
-    ])
+gulp.task('transfer:packagejson', () =>
+    gulp.src(['./package.json'])
     .pipe(gulpSSH.dest(config.dest)));
 
-gulp.task('transfer', () => 
+gulp.task('transfer', () =>
     runSequence('transfer:src', 'transfer:packagejson'));
 
-gulp.task('install', () => 
+gulp.task('install', () =>
     gulpSSH.shell([
         `cd ~/${config.dest}`,
         `npm install --production`
     ]));
 
-gulp.task('run', () =>
-    gulpSSH.shell(`sudo node ~/${config.dest}`));
-
 gulp.task('deploy', () =>
     runSequence('clean', 'transfer', 'install'));
+
+gulp.task('run', () =>
+    gulpSSH.shell(`sudo node ~/${config.dest}`));
